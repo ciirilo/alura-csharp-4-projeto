692ac1aa74f71ae7a0e9a12d0573ddf455ff4afe
pmachado.new <69328075+ciirilo@users.noreply.github.com>
2022-01-18 11:26:53 -0300

    atualizando banco

--- UsuariosApi/Data/UserDbContext.cs
+++ UsuariosApi/Data/UserDbContext.cs
@@ -17,43 +17,7 @@ namespace UsuariosApi.Data
             _configuration = configuration;
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            CustomIdentityUser admin = new CustomIdentityUser
-            {
-                UserName = "admin",
-                NormalizedUserName = "ADMIN",
-                Email = "admin@admin.com",
-                NormalizedEmail = "ADMIN@ADMIN.COM",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                Id = 99999
-            };
-
-            PasswordHasher<CustomIdentityUser> hasher = new PasswordHasher<CustomIdentityUser>();
-
-            admin.PasswordHash = hasher.HashPassword(admin, 
-                _configuration.GetValue<string>("admininfo:password"));
-
-            builder.Entity<CustomIdentityUser>().HasData(admin);
-
-            builder.Entity<IdentityRole<int>>().HasData(
-                new IdentityRole<int> { Id = 99999, Name = "admin", NormalizedName = "ADMIN" }
-            );
-
-            builder.Entity<IdentityRole<int>>().HasData(
-                new IdentityRole<int> { Id = 99997, Name = "regular", NormalizedName = "REGULAR" }
-            );
-
-            builder.Entity<IdentityUserRole<int>>().HasData(
-                new IdentityUserRole<int> { RoleId = 99999, UserId = 99999 }
-                );
-
-
-
-        }
+        
 
     }
 }
--- UsuariosApi/Migrations/UserDbContextModelSnapshot.cs
+++ UsuariosApi/Migrations/UserDbContextModelSnapshot.cs
@@ -107,10 +107,10 @@ namespace UsuariosApi.Migrations
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<int>", b =>
                 {
                     b.Property<string>("LoginProvider")
-                        .HasColumnType("varchar(767)");
+                        .HasColumnType("varchar(100)");
 
                     b.Property<string>("ProviderKey")
-                        .HasColumnType("varchar(767)");
+                        .HasColumnType("varchar(100)");
 
                     b.Property<string>("ProviderDisplayName")
                         .HasColumnType("text");
@@ -153,10 +153,10 @@ namespace UsuariosApi.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("LoginProvider")
-                        .HasColumnType("varchar(767)");
+                        .HasColumnType("varchar(100)");
 
                     b.Property<string>("Name")
-                        .HasColumnType("varchar(767)");
+                        .HasColumnType("varchar(100)");
 
                     b.Property<string>("Value")
                         .HasColumnType("text");
